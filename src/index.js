0b85c2dc1263905dcd60097ce5a491ca68b4fc54
moproblems8 <mhasan1080@gmail.com>
2023-07-09 17:33:24 -0700

    testing, index.js. it works

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ function component() {
     const element = document.createElement('div');
   
     // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = 'Hello webpack';
   
     return element;
   }

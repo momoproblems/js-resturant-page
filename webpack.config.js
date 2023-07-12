e22642d08f59ee1fdf666d9ceea0f6e296ab88ca
moproblems8 <mhasan1080@gmail.com>
2023-07-12 09:20:59 -0700

    added file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,17 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            esModule: false, // this is a workaround for a bug in file-loader
+            name: "[name].[hash].[ext]", // this will use the original file
+            outputPath: "assets/imgs" // this will output the file in dist/assets/imgs
+          }
+        }
+      },
     ],
   },
 };

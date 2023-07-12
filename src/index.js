311cf04272bbac4f8c45f9bf90b5ae98f0a634fa
moproblems8 <mhasan1080@gmail.com>
2023-07-12 10:43:58 -0700

    mainTitle works!

--- src/index.js
+++ src/index.js
@@ -10,6 +10,32 @@ import milk from './assets/home/milk.jpg';
 import oliveOil from './assets/home/olive-oil.jpg';
 import onion from './assets/home/onion.jpg';
 import spinach from './assets/home/spinach.jpg';
-import tomato from './assets/home/tomato.jpg';
+import tomato from './assets/home/tomatoes.jpg';
 
 const mainElement = document.querySelector('main');
+const mainTitle = document.createElement('div');
+const foodGrid = document.createElement('div');
+
+function createMainElement() {
+    mainElement.classList.add('wrapper');
+    return mainElement;
+}
+
+function createMainTitle() {
+    mainTitle.setAttribute('id', 'mainTitle');
+    mainTitle.innerText = 'Here are some of our favorite ingredients that we cook with everyday :)';
+    return mainTitle;
+}
+
+function createFoodGrid() {
+    foodGrid.classList.add('food-grid');
+    return foodGrid;
+}
+
+function createPage() {
+    mainElement = createMainElement();
+    mainElement.appendChild(createMainTitle());
+    mainElement.appendChild(createFoodGrid());
+}
+
+createPage();

34f69104b32b0c414b91f88eb2669de553a56e58
moproblems8 <mhasan1080@gmail.com>
2023-07-12 09:29:19 -0700

    added necessary imports

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import './styles/style.css';
+
+import cilantro from './assets/home/cilantro.jpg';
+import coffee from './assets/home/coffee.jpg';
+import cumin from './assets/home/cumin.jpg'
+import egg from './assets/home/egg.jpg';
+import garlic from './assets/home/garlic.jpg';
+import lambMeat from './assets/home/lamb-meat.jpg';
+import milk from './assets/home/milk.jpg';
+import oliveOil from './assets/home/olive-oil.jpg';
+import onion from './assets/home/onion.jpg';
+import spinach from './assets/home/spinach.jpg';
+import tomato from './assets/home/tomato.jpg';
+
+const mainElement = document.querySelector('main');

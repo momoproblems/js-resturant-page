8d392a96d1c735139c0b30d920497110873331c3
moproblems8 <mhasan1080@gmail.com>
2023-07-12 12:19:54 -0700

    made lists of all food-card values

--- src/components/home/food-card.js
+++ src/components/home/food-card.js
@@ -14,6 +14,20 @@ import foodGrid from './food-grid'
 
 const foodCard = document.createElement('div');
 
+const images = {
+    cilantroImage,
+    coffeeImage,
+    cuminImage,
+    eggImage,
+    garlicImage,
+    lambMeatImage,
+    milkImage,
+    oliveOilImage,
+    onionImage,
+    spinachImage,
+    tomatoImage
+}
+
 const titles = {
     cilantroTitle: "Cilantro",
     coffeeTitle: "Coffee",
@@ -42,6 +56,39 @@ const descriptions = {
     tomatoDescription: "Juicy orbs of summer delight, bursting with tangy sweetness, lending their vibrant hue to salads and sauces alike."
 };
 
-function createCard(img) {
+const urls = {
+    cilantroURL: "https://en.wikipedia.org/wiki/Coriander",
+    coffeeURL: "https://en.wikipedia.org/wiki/Coffee",
+    cuminURL: "https://en.wikipedia.org/wiki/Cumin",
+    eggURL: "https://en.wikipedia.org/wiki/Egg_as_food",
+    garlicURL: "https://en.wikipedia.org/wiki/Garlic",
+    lambMeatURL: "https://en.wikipedia.org/wiki/Lamb_and_mutton",
+    milkURL: "https://en.wikipedia.org/wiki/Milk",
+    oliveOilURL: "https://en.wikipedia.org/wiki/Olive_oil",
+    onionURL: "https://en.wikipedia.org/wiki/Onion",
+    spinachURL: "https://en.wikipedia.org/wiki/Spinach",
+    tomatoURL: "https://en.wikipedia.org/wiki/Tomato"
+}
 
+function createCard(image, title, description, urls) {
+    foodCard.classList.add('food-card');
+    
+    const imageDiv = document.createElement('div');
+    imageDiv.classList.add('food-image')
+    const imageElement = document.createElement('img');
+    imageElement.src = image;
+    imageDiv.appendChild(imageElement);
+
+    const infoDiv = document.createElement('div');
+    infoDiv.classList.add('food-info');
+    const titleElement = document.createElement('h1');
+    titleElement.textContent = title;
+    const descElement = document.createElement('h2');
+    descElement.textContent = description
+    infoDiv.appendChild(titleElement);
+    infoDiv.appendChild(descElement);
+
+    const 
+
+    return foodCard;
 }

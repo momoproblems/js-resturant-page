373802275e8c52c644b987a0b6db69a51c1594a8
moproblems8 <mhasan1080@gmail.com>
2023-07-14 20:05:25 -0700

    updated index.js code

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import '../static/styles/reset.css'
 import '../static/styles/utils.css'
 import '../static/styles/header.css'
 import '../static/styles/home.css'
-import homePage from './components/home/food-grid';
+import '../static/styles/about.css'
 import headerElement from './components/header'
+import homePage from './components/home/food-grid';
+import aboutPage from './components/about/about';
+
 
 function createHomePage() {
     document.body.appendChild(homePage); 
+}
+
+function createAboutPage() {
+    document.body.appendChild(aboutPage);
 }
 
 function initializeSite() {
@@ -28,6 +35,7 @@ document.addEventListener('DOMContentLoaded', () => {
     aboutButton.addEventListener('click', () => {
         // window.location.href = '/about.html';
         removePage();
+        createAboutPage();
     })
 
     const homeButton = document.querySelector('#home');
